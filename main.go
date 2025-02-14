111a955efd8d8e4f582164772586a5cc8e324f48
weibo <1240219091@qq.com>
2025-02-14 11:18:35 +0800

    Revert "智能代理"
    This reverts commit 00a8cb9eb1afad861abc944cfb9677d7925822bf.

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ var mode string
 var wsServer string
 var clientSocks5Port int
 var clientJwtPrivateKeyFilePath string
-var clientProxyDomainFilePath string
-var forceWsProxy bool
 var serverPort string
 
 func main() {
@@ -20,15 +18,13 @@ func main() {
 	flag.StringVar(&wsServer, "ws", "ws://localhost:1323/ws", "websocket Server")
 	flag.IntVar(&clientSocks5Port, "csp", 1080, "client Socks5 Port")
 	flag.StringVar(&clientJwtPrivateKeyFilePath, "cjp", "", "client Jwt PrivateKey File Path")
-	flag.StringVar(&clientProxyDomainFilePath, "dp", "", "client Proxy Domain File Path")
-	flag.BoolVar(&forceWsProxy, "fwp", true, "流量是否全部走webSocket")
 	flag.StringVar(&serverPort, "sp", "1323", "server Socks5 Port,default 1323")
 	flag.Parse()
 
 	if mode == "s" {
 		server.Listen(serverPort)
 	} else if mode == "c" {
-		client.Listen(client.Config{WsServerAddr: wsServer, Socks5Port: clientSocks5Port, JwtPrivateKeyFilePath: clientJwtPrivateKeyFilePath, ClientProxyDomainFilePath: clientProxyDomainFilePath, ForceWsProxy: forceWsProxy})
+		client.Listen(client.Config{WsServerAddr: wsServer, Socks5Port: clientSocks5Port, JwtPrivateKeyFilePath: clientJwtPrivateKeyFilePath})
 	} else {
 		fmt.Println("不支持该运行模式")
 	}
